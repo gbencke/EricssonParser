9ccaa2f849d32db51d3ba4bc521ee7d5404716ab
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-11-11 01:52:47 +0000

    Using xargs...

--- src/Parser/run.sh
+++ src/Parser/run.sh
@@ -6,6 +6,8 @@
 # Created on Nov 10, 2019, 7:16:33 PM
 #
 
+echo $(date) >> ./time.txt
+
 export PARSER_DATA_FILE=./data/edff.txt
 export PARSER_TABLE_PREFIX=t_
 export PARSER_FIELD_PREFIX=f_
@@ -19,16 +21,10 @@ sudo docker start MYSQL_PARSER
 
 cd $PARSER_OUTPUT_FOLDER
 
-for FILE_TO_COPY in *.SQL
-do
-    echo $FILE_TO_COPY
-    sudo docker cp $FILE_TO_COPY MYSQL_PARSER:/
-done
-wait
+ls -1 *.SQL | xargs -n1 -P 64 -I {} sudo docker cp {}  MYSQL_PARSER:/ &
 
 sudo docker exec MYSQL_PARSER /bin/sh -c "mysql -uPARSER -pPARSER PARSER < /000.DDL.CREATE.SQL" &
 
-ls -1 *.DML.SQL | xargs -n1 -P 8 -I {} sudo docker exec MYSQL_PARSER /bin/sh -c "mysql -uPARSER -pPARSER PARSER < /{}" 
+ls -1 *.DML.SQL | xargs -n1 -P 64 -I {} sudo docker exec MYSQL_PARSER /bin/sh -c "mysql -uPARSER -pPARSER PARSER < /{}" 
 
-
-
+echo $(date) >> ./time.txt
