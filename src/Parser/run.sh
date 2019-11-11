8952d835ed2565df2ddb4df94776a8249338777a
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-11-11 02:14:39 +0000

    More fixes in the script...

--- src/Parser/run.sh
+++ src/Parser/run.sh
@@ -6,16 +6,19 @@
 # Created on Nov 10, 2019, 7:16:33 PM
 #
 
-echo $(date) >> ./time.txt
-
 export PARSER_DATA_FILE=./data/edff.txt
 export PARSER_TABLE_PREFIX=t_
 export PARSER_FIELD_PREFIX=f_
 export PARSER_OUTPUT_FOLDER=./output
 
 rm -f $PARSER_OUTPUT_FOLDER/*
+rm ./time.txt
+
+echo $(date) >> ./time.txt
 
 ./bin/parser
+
+echo $(date) >> ./time.txt
 
 sudo docker start MYSQL_PARSER
 
@@ -25,6 +28,9 @@ ls -1 *.SQL | xargs -n1 -P 64 -I {} sudo docker cp {}  MYSQL_PARSER:/ &
 
 sudo docker exec MYSQL_PARSER /bin/sh -c "mysql -uPARSER -pPARSER PARSER < /000.DDL.CREATE.SQL" &
 
-ls -1 *.DML.SQL | xargs -n1 -P 64 -I {} sudo docker exec MYSQL_PARSER /bin/sh -c "mysql -uPARSER -pPARSER PARSER < /{}" 
+ls -1 *.DML.SQL | xargs -n1 -P 96 -I {} sudo docker exec MYSQL_PARSER /bin/sh -c "mysql -uPARSER -pPARSER PARSER < /{}" 
+
+cd ..
 
 echo $(date) >> ./time.txt
+
