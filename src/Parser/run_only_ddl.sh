2486c77c61a7bd46f0c303fcacf5ec8b0e9de8a7
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-11-15 19:06:12 -0300

    Only for generation of ddl

---
+++ src/Parser/run_only_ddl.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# 
+# File:   run.sh
+# Author: gbencke
+#
+# Created on Nov 10, 2019, 7:16:33 PM
+#
+
+export PARSER_DATA_FILE=./data/edff.txt
+export PARSER_TABLE_PREFIX=t_
+export PARSER_FIELD_PREFIX=f_
+export PARSER_OUTPUT_FOLDER=./output
+
+rm -f $PARSER_OUTPUT_FOLDER/*
+
+./bin/parser
+
+sudo docker start MYSQL_PARSER
+
+cd $PARSER_OUTPUT_FOLDER
+
+ls -1 000.*.SQL | xargs -n1 -P 64 -I {} sudo docker cp {}  MYSQL_PARSER:/
+
+sudo docker exec MYSQL_PARSER /bin/sh -c "mysql -uPARSER -pPARSER PARSER < /000.DDL.CREATE.SQL" &
+
