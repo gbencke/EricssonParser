686f71db66897dcdde5a2562fd3e88a1682a6fad
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-11-15 16:21:39 -0300

    Command to only run the parser

---
+++ src/Parser/run_only_parser.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# 
+# File:   run.sh
+# Author: gbencke
+#
+# Created on Nov 10, 2019, 7:16:33 PM
+#
+
+export PARSER_DATA_FILE=./data/edff.txt
+export PARSER_TABLE_PREFIX=t_
+export PARSER_FIELD_PREFIX=f_
+export PARSER_OUTPUT_FOLDER=./output
+
+rm -f $PARSER_OUTPUT_FOLDER/*
+
+./bin/parser
