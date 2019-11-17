a491271c8daf1e4b9409c8124ef6634daf921a96
Guilherme Bencke <gbencke@benckesoftware.com.br>
2019-11-16 22:10:55 -0300

    Only interested on the schema for now

--- src/Parser/db/dump.sh
+++ src/Parser/db/dump.sh
@@ -7,6 +7,6 @@ sudo systemctl start docker
 sudo docker run --name MYSQL_PARSER -e MYSQL_ROOT_PASSWORD=PARSER1234 -e MYSQL_DATABASE=PARSER -e MYSQL_USER=PARSER -e MYSQL_PASSWORD=PARSER -d mysql:latest
 sudo docker run --name MYSQL_PARSER_ORIG -e MYSQL_ROOT_PASSWORD=PARSER1234 -e MYSQL_DATABASE=PARSER -e MYSQL_USER=PARSER -e MYSQL_PASSWORD=PARSER -d mysql:latest
 
-sudo docker exec MYSQL_PARSER mysqldump -uPARSER -pPARSER PARSER  > GENERATED.SQL
-sudo docker exec MYSQL_PARSER_ORIG mysqldump -uPARSER -pPARSER PARSER  > ORIG.SQL
+sudo docker exec MYSQL_PARSER mysqldump --no-data -uPARSER -pPARSER PARSER  > GENERATED.SQL
+sudo docker exec MYSQL_PARSER_ORIG mysqldump --no-data -uPARSER -pPARSER PARSER  > ORIG.SQL
 
